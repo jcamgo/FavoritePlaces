2c3d37473382b58ed95d2b6f9c4f4a5b6e3a1242
Jonny Camargo <jonnycamargo96@gmail.com>
2019-01-05 23:57:51 -0700

    Clean Code

--- FavoritePlaces/ViewControler/MapScreen.swift
+++ FavoritePlaces/ViewControler/MapScreen.swift
@@ -14,7 +14,6 @@ class MapScreen: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
     
-    
     let locationManager = CLLocationManager()
     let regionInMeters: Double = 10000
     
@@ -23,20 +22,17 @@ class MapScreen: UIViewController {
         checkLocationServices()
     }
     
-    
     func setupLocationManager() {
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
     }
     
-    
     func centerViewOnUserLocation() {
         if let location = locationManager.location?.coordinate {
             //let region = MKCoordinateRegion.init(center: location, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
-            mapView.setRegion(region, animated: true)
+            //mapView.setRegion(region, animated: true)
         }
     }
-    
     
     func checkLocationServices() {
         if CLLocationManager.locationServicesEnabled() {
@@ -46,7 +42,6 @@ class MapScreen: UIViewController {
             // Show alert letting the user know they have to turn this on.
         }
     }
-    
     
     func checkLocationAuthorization() {
         switch CLLocationManager.authorizationStatus() {
@@ -69,15 +64,13 @@ class MapScreen: UIViewController {
     }
 }
 
-
 extension MapScreen: CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let location = locations.last else { return }
         //let region = MKCoordinateRegion.init(center: location.coordinate, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
-        mapView.setRegion(region, animated: true)
+        //mapView.setRegion(region, animated: true)
     }
-    
     
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         checkLocationAuthorization()
